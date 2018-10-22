d32e925c12887214f2bc3c8b23393bf0a5456be4
Živa Štebljaj <ziva.stebljaj@student.fmf.uni-lj.si>
2018-10-22 14:07:07 +0200

    vaje 22.10.

--- 2-zajem-podatkov/vaje/macke.py
+++ 2-zajem-podatkov/vaje/macke.py
@@ -12,9 +12,9 @@ cats_frontpage_url = 'http://www.bolha.com/zivali/male-zivali/macke/'
 # mapa, v katero bomo shranili podatke
 cat_directory = 'macke_naprodaj'
 # ime datoteke v katero bomo shranili glavno stran
-frontpage_filename = 'macke.html'
+cat_frontpage_filename = 'macke_stran.html'
 # ime CSV datoteke v katero bomo shranili podatke
-csv_filename = 'macke.csv'
+cat_csv_filename = 'macke.csv'
 
 
 def download_url_to_string(url):
@@ -32,7 +32,6 @@ def download_url_to_string(url):
     else:
         return r.text
 
-
 def save_string_to_file(text, directory, filename):
     '''Write "text" to the file "filename" located in directory "directory",
     creating "directory" if necessary. If "directory" is the empty string, use
@@ -43,14 +42,27 @@ def save_string_to_file(text, directory, filename):
         file_out.write(text)
     return None
 
+def save_string_to_file1(text, directory, filename):
+    '''Write "text" to the file "filename" located in directory "directory",
+    creating "directory" if necessary. If "directory" is the empty string, use
+    the current directory.'''
+    os.makedirs(os.path.join(os.path.dirname(__file__), directory), exist_ok=True)
+    path = os.path.join(os.path.dirname(__file__), directory, filename)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None
+
 # Definirajte funkcijo, ki prenese glavno stran in jo shrani v datoteko.
 
 
 def save_frontpage(url):
     '''Save "cats_frontpage_url" to the file
     "cat_directory"/"frontpage_filename"'''
-    text = download_url_to string(cats_frontpage_url)
-    return save_string_to_file(text, cat_directory, frontpage_filename)
+    text = download_url_to_string(cats_frontpage_url)
+    if text is None:
+        print('downloading cats failed')
+    print('downloading cats, saved')
+    return save_string_to_file(text, cat_directory, cat_frontpage_filename)
 
 ###############################################################################
 # Po pridobitvi podatkov jih želimo obdelati.
@@ -59,7 +71,8 @@ def save_frontpage(url):
 
 def read_file_to_string(directory, filename):
     '''Return the contents of the file "directory"/"filename" as a string.'''
-    with open(filename) as file:
+    path = os.path.join(directory, filename)
+    with open(path, 'r', encoding='utf_8') as file:
         return file.read()
 
 # Definirajte funkcijo, ki sprejme niz, ki predstavlja vsebino spletne strani,
@@ -68,26 +81,32 @@ def read_file_to_string(directory, filename):
 # oglasa. Funkcija naj vrne seznam nizov.
 
 
-def page_to_ads(TODO):
+def page_to_ads(stran):
     '''Split "page" to a list of advertisement blocks.'''
-    return TODO
+    vzorec = re.compile(r'div class="ad"(.*?)<div class="clear">', re.DOTALL)
+    oglasi = re.findall(vzorec, stran)
+    return oglasi
 
 # Definirajte funkcijo, ki sprejme niz, ki predstavlja oglas, in izlušči
 # podatke o imenu, ceni in opisu v oglasu.
 
 
-def get_dict_from_ad_block(TODO):
+def get_dict_from_ad_block(blok):
     '''Build a dictionary containing the name, description and price
     of an ad block.'''
-    return TODO
+    vzorec = re.compile(r'title="(?P<name>.*?)"', re.DOTALL)
+    podatki = re.search(vzorec, blok)
+    slovar = podatki.groupdict()
+    return slovar
 
 # Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
 # besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
 # vseh oglasih strani.
 
 
-def ads_from_file(TODO):
+def ads_from_file(directory, filename):
     '''Parse the ads in filename/directory into a dictionary list.'''
+    stran = read_file_to_string(directory, filename)
     return TODO
 
 ###############################################################################
